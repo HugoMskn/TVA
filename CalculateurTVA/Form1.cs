74b5d49756f4c0af4bc3102670f13ac8c6410a24
Hugo Miskine <hugomiskine@protonmail.com>
2023-05-22 09:31:56 +0200

    Initial upload

---
+++ CalculateurTVA/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateurTVA
+{
+    public partial class Form1 : Form
+    {
+        public double TauxTVA = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TauxTVA = 20;
+            label1.Text = "Taux TVA : 20%";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TauxTVA = 10;
+            label1.Text = "Taux TVA : 10%";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            TauxTVA = 5.5;
+            label1.Text = "Taux TVA : 5,5%";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double prix = double.Parse(textBox1.Text);
+            double resultat = prix / (1 + TauxTVA / 100);
+            richTextBox1.AppendText("Prix HT : " + resultat +" Prix d'origine : " + prix +$" TVA : {prix - resultat}" + "\n");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double prix = double.Parse(textBox1.Text);
+            double resultat = prix * (1 + TauxTVA / 100);
+            richTextBox1.AppendText("Prix TTC : " + resultat + " Prix d'origine : " + prix +$" TVA : {prix - resultat}"+"\n");
+        }
+    }
+}
